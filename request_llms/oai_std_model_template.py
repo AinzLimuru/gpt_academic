4c7945793e8f29dcf0a8b9d4aa42b98d0f412f44
Ooalgown Ainz <ooalgowna461@gmail.com>
2025-01-18 22:38:43 +0800

    修复生成卡死的问题

--- request_llms/oai_std_model_template.py
+++ request_llms/oai_std_model_template.py
@@ -194,53 +194,66 @@ def get_predict_function(
                     raise TimeoutError
                 if MAX_RETRY != 0:
                     logger.error(f"请求超时，正在重试 ({retry}/{MAX_RETRY}) ……")
+                    continue # 重试
 
-        stream_response = response.iter_lines()
-        result = ""
-        finish_reason = ""
-        while True:
+            stream_response = response.iter_lines()
+            result = ""
+            finish_reason = ""
             try:
-                chunk = next(stream_response)
-            except StopIteration:
-                if result == "":
-                    raise RuntimeError(f"获得空的回复，可能原因:{finish_reason}")
-                break
-            except requests.exceptions.ConnectionError:
-                chunk = next(stream_response)  # 失败了，重试一次？再失败就没办法了。
-            response_text, finish_reason = decode_chunk(chunk)
-            # 返回的数据流第一次为空，继续等待
-            if response_text == "" and finish_reason != "False":
-                continue
-            if response_text == "API_ERROR" and (
-                finish_reason != "False" or finish_reason != "stop"
-            ):
-                chunk = get_full_error(chunk, stream_response)
-                chunk_decoded = chunk.decode()
-                logger.error(chunk_decoded)
-                raise RuntimeError(
-                    f"API异常,请检测终端输出。可能的原因是:{finish_reason}"
-                )
-            if chunk:
-                try:
-                    if finish_reason == "stop":
-                        if not console_slience:
-                            print(f"[response] {result}")
+                while True:
+                    try:
+                        chunk = next(stream_response)
+                    except StopIteration:
+                        if result == "":
+                            raise RuntimeError(f"获得空的回复，可能原因:{finish_reason}")
                         break
-                    result += response_text
-                    if observe_window is not None:
-                        # 观测窗，把已经获取的数据显示出去
-                        if len(observe_window) >= 1:
-                            observe_window[0] += response_text
-                        # 看门狗，如果超过期限没有喂狗，则终止
-                        if len(observe_window) >= 2:
-                            if (time.time() - observe_window[1]) > watch_dog_patience:
-                                raise RuntimeError("用户取消了程序。")
-                except Exception as e:
-                    chunk = get_full_error(chunk, stream_response)
-                    chunk_decoded = chunk.decode()
-                    error_msg = chunk_decoded
-                    logger.error(error_msg)
-                    raise RuntimeError("Json解析不合常规")
+                    except requests.exceptions.ConnectionError as e: # 响应超时和读取超时的异常
+                        raise e
+                    response_text, finish_reason = decode_chunk(chunk)
+                    # 返回的数据流第一次为空，继续等待
+                    if response_text == "" and finish_reason != "False":
+                        continue
+                    if response_text == "API_ERROR" and (
+                        finish_reason != "False" or finish_reason != "stop"
+                    ):
+                        chunk = get_full_error(chunk, stream_response)
+                        chunk_decoded = chunk.decode()
+                        logger.error(chunk_decoded)
+                        raise RuntimeError(
+                            f"API异常,请检测终端输出。可能的原因是:{finish_reason}"
+                        )
+                    if chunk:
+                        try:
+                            if finish_reason == "stop":
+                                if not console_slience:
+                                    print(f"[response] {result}")
+                                break
+                            result += response_text
+                            if observe_window is not None:
+                                # 观测窗，把已经获取的数据显示出去
+                                if len(observe_window) >= 1:
+                                    observe_window[0] += response_text
+                                # 看门狗，如果超过期限没有喂狗，则终止
+                                if len(observe_window) >= 2:
+                                    if (time.time() - observe_window[1]) > watch_dog_patience:
+                                        raise RuntimeError("用户取消了程序。")
+                        except Exception as e:
+                            chunk = get_full_error(chunk, stream_response)
+                            chunk_decoded = chunk.decode()
+                            error_msg = chunk_decoded
+                            logger.error(error_msg)
+                            raise RuntimeError("Json解析不合常规")
+            except Exception as e:
+                retry += 1
+                traceback.print_exc()
+                if retry > MAX_RETRY:
+                    raise TimeoutError
+                if MAX_RETRY != 0:
+                    # 恢复到没有读取的状态
+                    observe_window[0] = ""
+                    result = ""
+                    logger.error(f"请求超时，正在重试 ({retry}/{MAX_RETRY}) ……")
+                    continue # 重试
         return result
 
     def predict(
